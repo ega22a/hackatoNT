cd22900082d786977b718359439199abbd5bc51f
porenti <72981212+porenti@users.noreply.github.com>
2020-10-17 19:30:19 +0500

    Не понимаю

--- backend/python/cgi-bin/gate.py
+++ backend/python/cgi-bin/gate.py
@@ -1,6 +1,6 @@
 from search_function import search_users
 
-f = open('1.txt','r')
+f = open('token.txt','r')
 token_vk = f.read()
 def gate_function(town,sex,token=token_vk):
         town = town[:13]
